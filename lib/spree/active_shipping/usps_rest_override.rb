faa18fe53b7103740f0edf7340de97a39e111f09
Gustavo Camello <gucamello@gmail.com>
2024-08-01 15:00:33 +0100

    wip

---
+++ lib/spree/active_shipping/usps_rest_override.rb
@@ -0,0 +1,18 @@
+module Spree
+  module ActiveShipping
+    module UpsRestOverride
+      def self.included(base)
+        base.class_eval do
+          def find_rates(origin, destination, packages, options={})
+            # origin, destination = upsified_location(origin), upsified_location(destination)
+            # options = @options.merge(options)
+            # packages = Array(packages)
+            # rate_request = build_rate_request(origin, destination, packages, options)
+            # response = commit(:rates, save_request(rate_request), (options[:test] || false))
+            # parse_rate_response(origin, destination, packages, response, options)
+          end
+        end
+      end
+    end
+  end
+end
