68781ed51aede50e455d5c9cccaf256978916091
Gustavo Camello <gucamello@gmail.com>
2024-08-07 13:08:32 +0100

    wip

--- app/models/spree/calculator/shipping/usps_rest/base.rb
+++ app/models/spree/calculator/shipping/usps_rest/base.rb
@@ -38,62 +38,6 @@ module Spree
             access_token: Spree::ActiveShipping::Config[:usps_access_token],
           )
         end
-
-        private
-
-        # def retrieve_rates(origin, destination, shipment_packages)
-        #   begin
-        #     response = carrier.find_rates(origin, destination, shipment_packages, rate_options)
-        #     raise response.inspect
-        #     # turn this beastly array into a nice little hash
-        #     # service_code_prefix_key = response.params.keys.first == 'IntlRateV2Response' ? :international : :domestic
-        #     service_code_prefix_key = :domestic
-
-        #     rates = response.rates do |rate|
-        #       service_code = "#{SERVICE_CODE_PREFIX[service_code_prefix_key]}:#{rate[:service_code]}"
-        #       [service_code, rate[:price]]
-        #     end
-            
-        #     rate_hash = Hash[*rates.flatten]
-
-        #     return rate_hash
-        #   rescue ::ActiveShipping::Error => e
-
-        #     if e.class == ::ActiveShipping::ResponseError && e.response.is_a?(::ActiveShipping::Response)
-        #       params = e.response.params
-        #       if params.has_key?("Response") && params["Response"].has_key?("Error") && params["Response"]["Error"].has_key?("ErrorDescription")
-        #         message = params["Response"]["Error"]["ErrorDescription"]
-        #       # Canada Post specific error message
-        #       elsif params.has_key?("eparcel") && params["eparcel"].has_key?("error") && params["eparcel"]["error"].has_key?("statusMessage")
-        #         message = e.response.params["eparcel"]["error"]["statusMessage"]
-        #       else
-        #         message = e.message
-        #       end
-        #     else
-        #       message = e.message
-        #     end
-
-        #     error = Spree::ShippingError.new("#{I18n.t(:shipping_error)}: #{message}")
-        #     Rails.cache.write @cache_key, error #write error to cache to prevent constant re-lookups
-        #     raise error
-        #   end
-        # end
-
-        # protected
-        
-        # def max_weight_for_country(country)
-        #   1120  # 70 lbs
-        # end
-
-        # def rate_options
-        #   if Spree::ActiveShipping::Config[:usps_commercial_plus]
-        #     { commercial_plus: true }
-        #   elsif Spree::ActiveShipping::Config[:usps_commercial_base]
-        #     { commercial_base: true }
-        #   else
-        #     {}
-        #   end
-        # end
       end
     end
   end
