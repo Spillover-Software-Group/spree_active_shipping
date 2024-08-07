3f07a03d92ef194a9c3ca8851b41407b1f7fc69b
Gustavo Camello <gucamello@gmail.com>
2024-08-07 11:25:19 +0100

    wip

--- app/models/spree/calculator/shipping/usps_rest/base.rb
+++ app/models/spree/calculator/shipping/usps_rest/base.rb
@@ -18,9 +18,10 @@ module Spree
           
           return nil if rates_result.kind_of?(Spree::ShippingError)
           return nil if rates_result.empty?
+          # rate = rates_result[self.class.service_code]
           rate = rates_result[self.class.description]
 
-          # raise "rates_result: #{rates_result} / rate: #{rate} and self: #{self} and class: #{self.class} class.service_code: #{self.class.service_code}".inspect
+          raise "rates_result: #{rates_result} / rate: #{rate} and self: #{self} and class: #{self.class} class.service_code: #{self.class.service_code}".inspect
 
           return nil unless rate
           rate = rate.to_f + (Spree::ActiveShipping::Config[:handling_fee].to_f || 0.0)
