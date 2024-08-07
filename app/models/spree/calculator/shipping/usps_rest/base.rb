c8ca2ddc4f81757a502f7548a95a55dd6b0d5887
Gustavo Camello <gucamello@gmail.com>
2024-08-07 09:56:29 +0100

    wip

--- app/models/spree/calculator/shipping/usps_rest/base.rb
+++ app/models/spree/calculator/shipping/usps_rest/base.rb
@@ -15,7 +15,6 @@ module Spree
           destination = build_location(order.ship_address)
 
           rates_result = retrieve_rates_from_cache(package, origin, destination)
-          raise "rates_result #{rates_result}".inspect
           
           return nil if rates_result.kind_of?(Spree::ShippingError)
           return nil if rates_result.empty?
