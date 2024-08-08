023e56944f01e36508b6db92ddd71b0ccf967294
Gustavo Camello <gucamello@gmail.com>
2024-08-08 10:07:23 +0100

    wip

--- app/models/spree/calculator/shipping/usps_rest/base.rb
+++ app/models/spree/calculator/shipping/usps_rest/base.rb
@@ -39,6 +39,41 @@ module Spree
             access_token: Spree::ActiveShipping::Config[:usps_access_token],
           )
         end
+
+        def retrieve_rates(origin, destination, shipment_packages)
+          begin
+            response = carrier.find_rates(origin, destination, shipment_packages, rate_options)
+            raise "Response: #{response}".inspect
+            # turn this beastly array into a nice little hash
+            service_code_prefix_key = response.params.keys.first == 'IntlRateV2Response' ? :international : :domestic
+            rates = response.rates.collect do |rate|
+              service_code = "#{SERVICE_CODE_PREFIX[service_code_prefix_key]}:#{rate.service_code}"
+              [service_code, rate.price]
+            end
+            rate_hash = Hash[*rates.flatten]
+            return rate_hash
+          rescue ::ActiveShipping::Error => e
+
+            if e.class == ::ActiveShipping::ResponseError && e.response.is_a?(::ActiveShipping::Response)
+              params = e.response.params
+              if params.has_key?("Response") && params["Response"].has_key?("Error") && params["Response"]["Error"].has_key?("ErrorDescription")
+                message = params["Response"]["Error"]["ErrorDescription"]
+              # Canada Post specific error message
+              elsif params.has_key?("eparcel") && params["eparcel"].has_key?("error") && params["eparcel"]["error"].has_key?("statusMessage")
+                message = e.response.params["eparcel"]["error"]["statusMessage"]
+              else
+                message = e.message
+              end
+            else
+              message = e.message
+            end
+
+            error = Spree::ShippingError.new("#{I18n.t(:shipping_error)}: #{message}")
+            Rails.cache.write @cache_key, error #write error to cache to prevent constant re-lookups
+            raise error
+          end
+
+        end
       end
     end
   end
