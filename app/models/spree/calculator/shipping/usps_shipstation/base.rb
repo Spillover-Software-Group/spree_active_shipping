7a1ccd066ef8d8b3eaf03c2369c690c6e2317069
Gustavo Camello <gucamello@gmail.com>
2024-08-20 13:59:14 +0100

    wip

--- app/models/spree/calculator/shipping/usps_shipstation/base.rb
+++ app/models/spree/calculator/shipping/usps_shipstation/base.rb
@@ -35,6 +35,8 @@ module Spree
         def retrieve_rates(origin, destination, shipment_packages)
           begin
             response = carrier.find_rates(origin, destination, shipment_packages)
+
+            raise "THE RATES #{response.rates}.inspect"
             # turn this beastly array into a nice little hash
             rates = response.rates.collect do |rate|
               next unless rate
