1990540be1307c08658b14565019cb630fb0e8e9
Gustavo Camello <gucamello@gmail.com>
2024-10-08 15:23:49 +0100

    wip

--- app/models/spree/calculator/shipping/usps_rest/base.rb
+++ app/models/spree/calculator/shipping/usps_rest/base.rb
@@ -41,7 +41,7 @@ module Spree
         end
 
         def retrieve_rates(origin, destination, shipment_packages)
-          # begin
+          begin
             response = carrier.find_rates(origin, destination, shipment_packages)
             # turn this beastly array into a nice little hash
             rates = response.rates.collect do |rate|
@@ -52,26 +52,26 @@ module Spree
             end
             rate_hash = Hash[*rates.flatten]
             return rate_hash
-          # rescue ::ActiveShipping::Error => e
+          rescue ::ActiveShipping::Error => e
 
-          #   if e.class == ::ActiveShipping::ResponseError && e.response.is_a?(::ActiveShipping::Response)
-          #     params = e.response.params
-          #     if params.has_key?("Response") && params["Response"].has_key?("Error") && params["Response"]["Error"].has_key?("ErrorDescription")
-          #       message = params["Response"]["Error"]["ErrorDescription"]
-          #     # Canada Post specific error message
-          #     elsif params.has_key?("eparcel") && params["eparcel"].has_key?("error") && params["eparcel"]["error"].has_key?("statusMessage")
-          #       message = e.response.params["eparcel"]["error"]["statusMessage"]
-          #     else
-          #       message = e.message
-          #     end
-          #   else
-          #     message = e.message
-          #   end
+            if e.class == ::ActiveShipping::ResponseError && e.response.is_a?(::ActiveShipping::Response)
+              params = e.response.params
+              if params.has_key?("Response") && params["Response"].has_key?("Error") && params["Response"]["Error"].has_key?("ErrorDescription")
+                message = params["Response"]["Error"]["ErrorDescription"]
+              # Canada Post specific error message
+              elsif params.has_key?("eparcel") && params["eparcel"].has_key?("error") && params["eparcel"]["error"].has_key?("statusMessage")
+                message = e.response.params["eparcel"]["error"]["statusMessage"]
+              else
+                message = e.message
+              end
+            else
+              message = e.message
+            end
 
-          #   error = Spree::ShippingError.new("#{I18n.t(:shipping_error)}: #{message}")
-          #   Rails.cache.write @cache_key, error #write error to cache to prevent constant re-lookups
-          #   raise error
-          # end
+            error = Spree::ShippingError.new("#{I18n.t(:shipping_error)}: #{message}")
+            Rails.cache.write @cache_key, error #write error to cache to prevent constant re-lookups
+            raise error
+          end
         end
 
         protected
