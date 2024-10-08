9cb0fe9555e2e8462828462c548e07a3ea609799
Gustavo Camello <gucamello@gmail.com>
2024-10-08 11:31:36 +0100

    wip

--- app/models/spree/calculator/shipping/active_shipping/base.rb
+++ app/models/spree/calculator/shipping/active_shipping/base.rb
@@ -210,12 +210,13 @@ module Spree
           return [] unless package.contents.one?
 
           variant = package.contents.first.variant
+          raise "the variant #{variant} and check this #{variant&.width}"
           [variant.width, variant.depth, variant.height]
         end
 
         # Generates an array of Package objects based on the quantities and weights of the variants in the line items
         def packages(package)
-          raise "THE PACKAGE #{package&.dimension} and the contents = #{package&.contents}".inspect
+          # raise "THE PACKAGE #{package&.dimension} and the contents = #{package&.contents}".inspect
           units = Spree::ActiveShipping::Config[:units].to_sym
           packages = []
           weights = convert_package_to_weights_array(package)
