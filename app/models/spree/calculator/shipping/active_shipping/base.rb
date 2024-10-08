72d8e8196b9a611789671a2b98b19100ef1f4abf
Gustavo Camello <gucamello@gmail.com>
2024-10-08 12:52:07 +0100

    wip

--- app/models/spree/calculator/shipping/active_shipping/base.rb
+++ app/models/spree/calculator/shipping/active_shipping/base.rb
@@ -200,7 +200,7 @@ module Spree
               end
             else
               quantity.times do
-                packages << [variant.weight * multiplier, variant&.length, variant.width, variant.height]
+                packages << [variant.weight * multiplier, variant&.depth, variant&.width, variant&.height]
               end
             end
           end
