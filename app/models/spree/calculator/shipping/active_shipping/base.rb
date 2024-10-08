088f1b5dd90e78d4a89cd872dab58c7941065024
Gustavo Camello <gucamello@gmail.com>
2024-10-08 13:27:01 +0100

    wip

--- app/models/spree/calculator/shipping/active_shipping/base.rb
+++ app/models/spree/calculator/shipping/active_shipping/base.rb
@@ -300,7 +300,7 @@ module Spree
         def retrieve_rates_from_cache package, origin, destination
           Rails.cache.fetch(cache_key(package)) do
             shipment_packages = packages(package)
-            raise "the shipment_packages = #{shipment_packages} and #{shipment_packages[0].inches(:height)}".inspect
+            raise "#{shipment_packages[0].inches(:height)} and #{shipment_packages[1].inches(:width)} and #{shipment_packages[2].inches(:length)} and ".inspect
             if shipment_packages.empty?
               {}
             else
