92aaf4286bc93b0f3250593371e0083c5f7c9e6a
Gustavo Camello <gucamello@gmail.com>
2026-03-03 10:40:33 +0000

    wip

--- app/models/spree/calculator/shipping/fedex_rest/base.rb
+++ app/models/spree/calculator/shipping/fedex_rest/base.rb
@@ -35,7 +35,6 @@ module Spree
             client_secret = ENV['FEDEX_CLIENT_SECRET']
           end
 
-          raise "from carrier GUSTAVO"
           ::ActiveShipping::FedexRest.new(client_id:, client_secret:, client_account:)
         end
 
