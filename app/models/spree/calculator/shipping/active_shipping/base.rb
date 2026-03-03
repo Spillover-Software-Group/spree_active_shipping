b65a3d3b421ed63e30512f050acd1258d7030d2a
Gustavo Camello <gucamello@gmail.com>
2026-03-03 10:56:33 +0000

    wip

--- app/models/spree/calculator/shipping/active_shipping/base.rb
+++ app/models/spree/calculator/shipping/active_shipping/base.rb
@@ -287,12 +287,14 @@ module Spree
 
         def retrieve_rates_from_cache package, origin, destination
           # Rails.cache.fetch(cache_key(package)) do
-            shipment_packages = packages(package)
-            if shipment_packages.empty?
-              {}
-            else
-              retrieve_rates(origin, destination, shipment_packages)
-            end
+          retrieve_rates(origin, destination, shipment_packages)
+
+            # shipment_packages = packages(package)
+            # if shipment_packages.empty?
+            #   {}
+            # else
+            #   retrieve_rates(origin, destination, shipment_packages)
+            # end
           # end
         end
       end
